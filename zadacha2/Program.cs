b6e4d855b83d9f32d50a6cf781323712c5c50b8b
Vladimir <doe@example.com>
2023-12-02 19:50:55 +0300

    задача2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Задайте массив на 10 целых чисел. Напишите программу,
+// которая определяет количество чётных чисел в массиве.
+
+int[] array = { 6, 7, 19, 34, 3, 1, 4, 7, 9, 1 };
+Console.WriteLine("Массив: ");
+PrintArray(array);
+int count = 0;
+for (int a = 0; a < array.Length; a++)
+    if (array[a] % 2 == 0)
+        count++;
+Console.WriteLine($"Чётных чисел: {count}");
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " | ");
+    }
+    Console.WriteLine();
+}
